4b5c3c436d2abbb1ec4d32abf589413fd9920bd0
Robera Worku <robera.worku@klarna.com>
2023-10-20 08:35:12 +0200

    Adding Static portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,164 @@
-import Image from 'next/image'
+import Image from "next/image";
+
+type PortfolioLink = { name: string; link: string; content: string };
+type Skill = { name: string; subSkill: string[] };
+type WorkHistory = {
+  company: string;
+  from: Date;
+  to: Date | "now";
+  positions: [{ positionTitle: string; acheivments: string[] }];
+};
+type EducationHistory = {
+  school: string;
+  level: "Masters" | "Bachelors";
+  course: string;
+  gpa: number;
+  awards: string[];
+  from: Date;
+  to: Date | "now";
+};
+
+function SectionHeaderText(props) {
+  const { children } = props;
+  return <h4 className="pb-5 text-xl font-semibold">{children}</h4>;
+}
 
 export default function Home() {
+  const links: PortfolioLink[] = [
+    { name: "Email", link: "", content: "robgogoworku@gmail.com" },
+    { name: "Github", link: "https://github.com/Robgogo", content: "Robgogo" },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/in/robera-worku/",
+      content: "robera-worku",
+    },
+  ];
+
+  const skills: Skill[] = [
+    {
+      name: "Javascript/Typescript",
+      subSkill: ["Node.js", "NestJS🥰", "React", "React Native"],
+    },
+    {
+      name: "Python",
+      subSkill: [
+        "Django",
+        "Django REST🥰",
+        "Flask",
+        "Fast API␆",
+        "Data Analysis tools like Pandas, numpy ...",
+      ],
+    },
+    { name: "Java", subSkill: ["Spring Boot"] },
+    { name: "CI/CD", subSkill: ["Jenkins", "Github Actions"] },
+    { name: "Database", subSkill: ["PostgreSQL", "MySQL", "Mongodb"] },
+    { name: "Cloud", subSkill: ["AWS", "GCP"] },
+    { name: "Misc", subSkill: ["GIT", "Docker", "Kubernetes␆"] },
+  ];
+
+  const workHistory: WorkHistory[] = [
+    {
+      company: "Klarna",
+      from: new Date("2021/12/01"),
+      to: "now",
+      positions: [
+        {
+          positionTitle: "Graduate Software Engineer",
+          acheivments: ["Worked on.."],
+        },
+      ],
+    },
+  ];
+
+  const educationHistory: EducationHistory[] = [
+    {
+      school: "IE University",
+      level: "Masters",
+      course: "Master in Computer Science and Business Technology",
+      awards: ["IE Kistefos Full Scholarship for Young Talented"],
+      from: new Date("2020/09/01"),
+      to: new Date("2021/07/23"),
+      gpa: 3.74,
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col gap-5 p-4 mx-auto max-w-[900px] w-full">
+      <h2 className="font-bold text-2xl md:text-3xl sm:text-4xl">
+        My Portfolio
+      </h2>
+      <section>
+        <SectionHeaderText>
+          Robera Worku - Computer/Software Engineer, Cybersecurity Enthusiast
+        </SectionHeaderText>
+        <div className="flex flex-col gap-3">
+          {links.map((link, index) => (
+            <div
+              key={index}
+              className="relative group overflow-hidden flex items-center gap-1.5 flex-wrap text-xs sm:text-sm"
+            >
+              <div className="bg-purple-600 duration-400 group-hover:translate-x-full h-[2px] w-full absolute right-full bottom-0"></div>
+              <p>
+                <b>{link.name}</b>
+              </p>
+              <a
+                href={link.link}
+                target="_blank"
+                className="text-sky-400 cursor-pointer"
+              >
+                {link.content}
+              </a>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      <section>
+        <SectionHeaderText>Skills</SectionHeaderText>
+        <div className="flex flex-col gap-3">
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="relative group overflow-hidden flex items-center gap-1.5 flex-wrap text-xs sm:text-sm"
+            >
+              <div className="bg-purple-600 duration-400 group-hover:translate-x-full h-[2px] w-full absolute right-full bottom-0"></div>
+              <p>
+                <b>{skill.name}</b>
+              </p>
+              <p>{skill.subSkill.join(", ")}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section>
+        <SectionHeaderText> Work History</SectionHeaderText>
+        <div className="flex gap-3">
+          {workHistory.map((work, index) => (
+            <div key={index} className="text-xs sm:text-sm">
+              <p>
+                <b>{work.company}</b> - ({work.from.toDateString()} -{" "}
+                {work.to.toLocaleString()})
+              </p>
+              <div>
+                <ul>
+                  {work.positions.map((position, index) => (
+                    <li key={index}>
+                      <p>
+                        <b>{position.positionTitle}</b>
+                        <ul>
+                          {position.acheivments.map((achievment, index) => (
+                            <li key={index}>{achievment}</li>
+                          ))}
+                        </ul>
+                      </p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section></section>
     </main>
-  )
+  );
 }
